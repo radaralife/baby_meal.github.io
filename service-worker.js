4fe0bbccf6699d776e7356580248391c47e29717
radaralife <122142452+radaralife@users.noreply.github.com>
2023-07-26 01:18:50 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/baby_meal.github.io/precache-manifest.8f439cccade37473f58e10b82bff610d.js"
+  "/baby_meal.github.io/precache-manifest.476ce683b9c653eeeb5787545f30e899.js"
 );
 
 workbox.clientsClaim();
